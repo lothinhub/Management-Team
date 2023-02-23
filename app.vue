677cff18c8fcfad3d2ca8994ee2b5d778d6f562f
lothin <lothin5000000@gmail.com>
2023-02-23 15:37:30 +0700

    Nuxt3

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <LayOut />
+  </div>
+</template>
+
+<script lang="ts">
+import LayOut from './View/Layout/LayOut.vue';
+export default {
+  components: {
+    LayOut,
+  }
+}
+</script>
+
+<style></style>
